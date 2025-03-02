47c2d53f75f0cf3abeeeb610e3ed8dda68751d4f
SaimaAbhii <abhiiram.as@gmail.com>
2025-03-02 15:50:02 +0530

    modify main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
-#creating a VPC
+# Create a VPC
 resource "aws_vpc" "ecommerce_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "ecommerce-vpc"
   }
 }
 
-#creating a securty group for web server
+# Create a security group for the web server
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.ecommerce_vpc.id
   name   = "webserverSG"
@@ -29,12 +32,13 @@ resource "aws_security_group" "web_sg" {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "ecommerce-webserver-sg"
   }
 }
 
-#create a security group for database server
+# Create a security group for the database server
 resource "aws_security_group" "db_sg" {
   vpc_id = aws_vpc.ecommerce_vpc.id
   name   = "DBServerSG"
@@ -43,7 +47,7 @@ resource "aws_security_group" "db_sg" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
+    security_groups = [aws_security_group.web_sg.id] # Only web server can access DB
   }
 
   egress {
@@ -54,11 +58,11 @@ resource "aws_security_group" "db_sg" {
   }
 
   tags = {
-    Name = "ecommercce-dbserver-sg"
+    Name = "ecommerce-dbserver-sg"
   }
 }
 
-#creating a public subnet inside the VPC
+# Create a public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.ecommerce_vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -70,7 +74,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-#creating a private subnet inside the VPC
+# Create a private subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.ecommerce_vpc.id
   cidr_block        = "10.0.2.0/24"
@@ -81,7 +85,7 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-#creating an internet Gateway for the VPC
+# Create an Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.ecommerce_vpc.id
 
@@ -90,7 +94,22 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-#creating a Route table for the public subnet
+# Create a NAT Gateway Elastic IP
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+# Create a NAT Gateway in the public subnet
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "ecommerce-nat-gateway"
+  }
+}
+
+# Create a Route Table for the public subnet
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.ecommerce_vpc.id
 
@@ -104,35 +123,59 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Associate the public route table with the public subnet
 resource "aws_route_table_association" "public_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
+# Create a Route Table for the private subnet (for NAT Gateway)
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.ecommerce_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "ecommerce-private-route-table"
+  }
+}
+
+# Associate the private route table with the private subnet
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# AWS Provider
 provider "aws" {
   region = "ap-south-1"
 }
 
-#web server ec2 instance
+# Web Server EC2 Instance (Public)
 resource "aws_instance" "web_server" {
   ami                    = "ami-023a307f3d27ea427"
   instance_type          = "t2.micro"
   key_name               = "keypair"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+
   tags = {
-    Name = "tf-ecommerce-server"
+    Name = "tf-ecommerce-webserver"
   }
 }
 
-#database server ec2 instance
+# Database Server EC2 Instance (Private)
 resource "aws_instance" "db_server" {
   ami                    = "ami-023a307f3d27ea427"
   instance_type          = "t2.micro"
   key_name               = "keypair"
   subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.db_sg.id]
+
   tags = {
-    Name = "tf-databse-server"
+    Name = "tf-ecommerce-dbserver"
   }
 }
